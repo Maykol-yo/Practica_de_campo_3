7bede73e1fc614ff51c3aefdf4a4fa67485d4cb4
diego <diego@.com>
2026-05-04 11:46:06 -0500

    añadiendo primeras clases

---
+++ src/trabajo_de_campo/Trabajo_de_campo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabajo_de_campo;
+
+/**
+ *
+ * @author dusol
+ */
+public class Trabajo_de_campo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
