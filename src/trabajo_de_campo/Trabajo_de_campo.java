60def88468d898e953adfd28486653e21357a2f3
Lis <gallardodiazyessenialisbeth@gmail.com>
2026-05-04 20:56:41 -0500

    Commit Yessenia

--- src/trabajo_de_campo/Trabajo_de_campo.java
+++ src/trabajo_de_campo/Trabajo_de_campo.java
@@ -15,6 +15,7 @@ public class Trabajo_de_campo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("Holaaa");
     }
     
 }
